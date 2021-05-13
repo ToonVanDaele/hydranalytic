4bf64e118dbed20e612133d0941f8cd4cf3b890c
VAN DAELE <toon.vandaele@inbo.be>
2021-05-14 00:02:41 +0200

    add theis_r

---
+++ R/theis_r.R
@@ -0,0 +1,5 @@
+theis_r <- function(Q, t, s, Kh, D, S, W_u_method = "srivastava"){
+  uniroot(function(r, s) theis_s(Q = Q, r = r, t = t, Kh = Kh, D = D, S = S,
+                                 W_u_method = W_u_method) - s,
+          interval = c(1e-300, 1e30), s = s)
+}
