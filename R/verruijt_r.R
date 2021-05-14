efffda7ea27cc9d3c4e935ed9b5cb23db80d7e8c
VAN DAELE <toon.vandaele@inbo.be>
2021-05-14 15:03:30 +0200

    remove verruijt_0

--- R/verruijt_r.R
+++ R/verruijt_r.R
@@ -17,18 +17,7 @@
 #' @return r distance (L)
 
 #------------------------------------------------------------------------------
-radius_ThiemDupuitVerruijt <- function(r0, Q, D, Kh, Rech, s) {
-
-  r <- ifelse(ThiemDupuitVerruijt_0(r = 0.001, r0 = r0, Q = Q, D = D, Kh = Kh,
-                                    Rech = Rech, s = s) < 0,
-              uniroot(f = ThiemDupuitVerruijt_0, r0 = r0, Q = Q, D = D, Kh = Kh,
-                      Rech = Rech, s = s, lower = 0.001, upper = r0),
-              0.001)
-  return(r[[1]])
-}
-
-
-ThiemDupuitVerruijt_r <- function(r0, Q, D, Kh, Rech, s){
+radius_ThiemDupuitVerruijt  <- function(r0, Q, D, Kh, Rech, s){
 
   r <- uniroot(function(r, s0 = s) ThiemDupuitVerruijt(r = r, r0 = r0, Q = Q, D = 10, Kh = 5,
                                                   Rech = Rech) - s0,
