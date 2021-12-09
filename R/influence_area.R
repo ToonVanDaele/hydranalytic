96c4593a8e716d99fd3fa9d6121664b597c334ac
VAN DAELE <toon.vandaele@inbo.be>
2021-12-09 23:52:15 +0100

    change name

--- R/influence_area.R
+++ R/influence_area.R
@@ -1,19 +1,19 @@
-#' Area of influence
+#' Equal recharge area
 #'
-#' @description Area of influence for a stationary groundwater well in a freatic
-#' aquifer with net recharge. Reference: De Smedt, 2007
+#' @description The area for a stationary groundwater well were extraction
+#' equals net recharge. Reference: De Smedt, 2007
 #'
 #' @param Q abstraction (L^3/T)
 #' @param Rech recharge (L^3/T)
 #'
-#' @return area of influence (L^2)
+#' @return area (L^2)
 #'
 #' @examples
-#' a_freat_steady(50, 0.2)
+#' freat_steady_a(50, 0.2)
 #'
 #' @export
 
-a_freat_steady <- function(Q, Rech) {
+freat_equal_a <- function(Q, Rech) {
 
   A <- Q / Rech
   return(A)
--- R/theis_r.R
+++ R/theis_r.R
@@ -30,6 +30,6 @@
 theis_r <- function(Q, t, s, Kh, D, S, W_u_method = "srivastava"){
   r <- uniroot(function(r, s) theis_s(Q = Q, r = r, t = t, Kh = Kh, D = D, S = S,
                                  W_u_method = W_u_method) - s,
-          interval = c(1, 1e30), s = s)
+          interval = c(1e-30, 1e30), s = s)
   return(r[[1]])
 }
